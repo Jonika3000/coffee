a7bebee14ac95bca99f8e53990267494ed31939a
jonika jamcom <katyadyduch@gmail.com>
2023-01-25 18:30:45 +0200

    upd

--- Scripts/menuScrolling.js
+++ Scripts/menuScrolling.js
@@ -1,18 +1,25 @@
-/* var stickyOffset = $('.flex-container').offset().top;
+var stickyOffset = $('.header').offset().top;
 
 $(window).scroll(function () {
-  var sticky = $('.flex-container'),
+  var sticky = $('.header'),
     scroll = $(window).scrollTop();
-
-  if (scroll >= stickyOffset)
+  var logo = $("#logo").first();
+ /*  if (scroll >= stickyOffset)
+  {
     sticky.addClass('fixed');
+  } 
   else
+  {
     sticky.removeClass('fixed');
+  } */
+   
   if (jQuery(window).scrollTop() > 150) {
     sticky.addClass('scrolled');
+    logo.attr("src","Images/LogoCup.svg"); 
     sticky.removeClass('notScroll');
   } else {
     sticky.addClass('notScroll');
+    logo.attr("src","Images/Logo.png"); 
     sticky.removeClass('scrolled');
   }
-}); */
+});
