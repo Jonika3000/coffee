76ee5b875be2a8c081ffb7d93ff5b76bf65417a9
Jonika <katyadyduch@gmail.com>
2023-01-25 12:35:24 +0200

    first commit

---
+++ Scripts/menuScrolling.js
@@ -0,0 +1,18 @@
+/* var stickyOffset = $('.flex-container').offset().top;
+
+$(window).scroll(function () {
+  var sticky = $('.flex-container'),
+    scroll = $(window).scrollTop();
+
+  if (scroll >= stickyOffset)
+    sticky.addClass('fixed');
+  else
+    sticky.removeClass('fixed');
+  if (jQuery(window).scrollTop() > 150) {
+    sticky.addClass('scrolled');
+    sticky.removeClass('notScroll');
+  } else {
+    sticky.addClass('notScroll');
+    sticky.removeClass('scrolled');
+  }
+}); */
